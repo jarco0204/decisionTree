a28554dce9c549f05167d969fe04480bfdc64d70
Johan <jarco0204@outlook.com>
2020-11-18 13:11:05 -0330

    depth2Done

--- program/start.py
+++ program/start.py
@@ -7,17 +7,42 @@ def main():
     trainData= np.loadtxt("../data/train.txt") #Each element of this ndarray is a row
     file = open("../data/dataDesc.txt","r")
     m = json.load(file)
-    file.close() #Efficient AF
-    # print(trainData)
+    file.close() 
+    
+    attributeSet, newDataSet= findRootNode(trainData,m)
+    for newData in newDataSet:
+        datasetEntropy= calculateGeneralEntropy(newData) # This is the entropy value for the whole dataset
+        gainAttributes= []
+        for attr in range(1,len(attributeSet)): #len(m)
+            gainAttributes.append(datasetEntropy-calculateEntropyAttribute(newData,attr))
+        arr= np.array(gainAttributes)
+        indexMax= np.argmax(arr,axis=0) + 1 # first element contains class label
+        print(" Node is %s" % m[indexMax][0])
+        attributeSet.pop(indexMax)
+    
+    
+
+
+    
+
+def findRootNode(trainData,m):
     datasetEntropy= calculateGeneralEntropy(trainData) # This is the entropy value for the whole dataset
     gainAttributes= []
-    for attr in range(1,6): #len(m)
-        gainAttributes.append(datasetEntropy-calculateEntropyAttribute(m[attr],trainData,attr))
+    for attr in range(1,len(m)): #len(m)
+        gainAttributes.append(datasetEntropy-calculateEntropyAttribute(trainData,attr))
     arr= np.array(gainAttributes)
     indexMax= np.argmax(arr,axis=0) + 1 # first element contains class label
     print("Root Node is %s" % m[indexMax][0])
     m.pop(indexMax)
 
+    newAr= buildModifiedTrainData(trainData,indexMax)
+    
+    return m, newAr
+
+#This is a helper function that is called for every internal node
+#@args the original trainining set and the index[1 to n] of the attribute to classify the dataset
+#@returns the modifed array according to the attribute labels [1..3]
+def buildModifiedTrainData(trainData,indexMax):
     #This new section creates the next array to work with
     indexesAttr= classifyByAttrLabels(trainData[indexMax]) #indexes used to create new array according to attribute labels
     trainData=np.delete(trainData,indexMax,0)
@@ -25,30 +50,15 @@ def main():
     for attrLabel in indexesAttr:
         newData=np.zeros(shape=(len(trainData[:,1]),len(attrLabel)))
         i=0
-        # print(newData)
         for element in attrLabel:
-            # print(trainData[:,element])
             newData[:,i]=trainData[:,element]
             i+=1
         newAr.append(newData)
-    # print(newAr[0])
+    return newAr
 
-    datasetEntropy= calculateGeneralEntropy(newAr[0]) # This is the entropy value for the whole dataset
-    gainAttributes= []
-    for attr in range(1,5): #len(m)
-        gainAttributes.append(datasetEntropy-calculateEntropyAttribute(None,newAr[0],attr))
-    arr= np.array(gainAttributes)
-    print(arr)
-    indexMax= np.argmax(arr,axis=0) + 1 # first element contains class label
-    print("Next Node is %s" % m[indexMax][0])
-    m.pop(indexMax)
-
-
-    
-
-    
-    
-
+#This function calculate the entropy of the whole dataset, in order to find the gain for each attribute.
+#@args data which is a ndnumpy array
+#@return the entropy for the dataset
 def calculateGeneralEntropy(data):
     classLabelRow= data[0] #First Row contains the class label entries
     lowRisk=0
@@ -64,10 +74,7 @@ def calculateGeneralEntropy(data):
     
 
 
-def calculateEntropyAttribute(attribute,data,attrIndex):
-    # print()
-    # print("Calculating entropy for %s" % attribute[0])
-    # print("It has %s classes" % len(attribute[1]))
+def calculateEntropyAttribute(data,attrIndex):
 
     #Classify each element according to their attribute class (for this project at most 3)
     row= data[attrIndex]
@@ -83,7 +90,7 @@ def calculateEntropyAttribute(attribute,data,attrIndex):
             else: #High risk
                 highRisk+=1
         classSummary.append([lowRisk,highRisk])
-    # print(classSummary)
+    print(classSummary)
 
     #Calculate the entropy for each attribute label
     entropyAttr=[]
@@ -93,7 +100,10 @@ def calculateEntropyAttribute(attribute,data,attrIndex):
         totalsAttr.append(total)
         firstTerm= attrLabel[0]/total
         secondTerm= attrLabel[1]/total
-        entropyAttrLabel= (-firstTerm * math.log(firstTerm,2))+(-secondTerm * math.log(secondTerm,2))
+        if(firstTerm!=0 and secondTerm!=0):
+            entropyAttrLabel= (-firstTerm * math.log(firstTerm,2))+(-secondTerm * math.log(secondTerm,2))
+        else:
+            entropyAttrLabel=0
         entropyAttr.append(entropyAttrLabel)
     
     finalEntropyAttribute=0
@@ -104,6 +114,9 @@ def calculateEntropyAttribute(attribute,data,attrIndex):
 
     return finalEntropyAttribute
 
+#This function classifies an attribute/row according to their labels
+#@args takes an array containing the attribute to classify
+#@return a 2D array containing the indexes of the class label
 def classifyByAttrLabels(row):
     attrLabels=[[],[],[]] #stores indexes for each attribute label
     for element in range(len(row)):
